5cd646fd8983d7ea8f9a298a68a662b333e027bc
male-andante <andreadanteprivitera@gmail.com>
2025-01-27 15:58:08 +0100

    Esercizio completato

--- js/netflix.js
+++ js/netflix.js
@@ -1,10 +1,11 @@
 window.addEventListener('scroll', function () {
 
-    const movies = document.querySelectorAll('.movieSection')
+    const watchAgain = document.querySelector('#watchAgain')
+    const newRelease = document.querySelector('#newRelease')
     const scrollPosition = document.documentElement.scrollTop
 
-    if (scrollPosition < 30) {
-        movies.forEach(movie => {
+    if (scrollPosition > 50) {
+        [watchAgain, newRelease].forEach(movie => {
             movie.style.visibility = "hidden";
             movie.style.opacity = '0';
             movie.style.pointerEvents = "none"; // Impedisce interazioni
@@ -12,12 +13,18 @@ window.addEventListener('scroll', function () {
         )
     }
     else {
-        movies.forEach(movie => {
-            movie.style.visibility = "visible";
-            movie.style.opacity = '1';
-            movie.style.pointerEvents = "auto"; // Ripristina le interazioni
-        }
-        )
+        watchAgain.style.visibility = "visible"
+        watchAgain.style.opacity = "1"
+        watchAgain.style.pointerEvents = "auto"
+        watchAgain.style.transition = "opacity 0.5s ease-in-out, visibility 0.5s"
+
+        setTimeout(() => {
+        newRelease.style.visibility = "visible"
+        newRelease.style.opacity = "1"
+        newRelease.style.pointerEvents = "auto"
+        newRelease.style.transition = "opacity 0.5s ease-in-out, visibility 0.5s"
+      }, 500) // Ritardo di 0.5 secondi
     }
 
-})
+    }
+)
